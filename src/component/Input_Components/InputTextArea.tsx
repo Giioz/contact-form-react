e79638930f44e03201f036ed5020269eabc79a5f
Mamaladze Gio <mamaladzegio99@gmail.com>
2024-09-12 11:22:30 +0400

    fix: fixed textarea

--- src/component/Input_Components/InputTextArea.tsx
+++ src/component/Input_Components/InputTextArea.tsx
@@ -2,7 +2,7 @@ export const InputTextArea = () => {
   return (
     <div>
         <label className="labelText block">Message <span className="text-[#0C7D69]">*</span></label>
-        <textarea className='inputSize min-h-[15rem] md:min-h-[8.25rem]'></textarea>
+        <textarea className='inputSize min-h-[15rem] resize-none md:min-h-[8.25rem]'></textarea>
     </div>
   )
 }
