8f7ad5f6f5dc38e23d28681b7ee10961c08b349b
Mamaladze Gio <mamaladzegio99@gmail.com>
2024-09-16 19:13:52 +0400

    fix

--- src/component/Input_Components/InputCheckbox.tsx
+++ src/component/Input_Components/InputCheckbox.tsx
@@ -5,7 +5,7 @@ export const InputCheckbox: React.FC<InputCheckboxProps> = ({value, onChange, er
     <>
       <div className="flex gap-[1rem] items-center mt-[1.3rem]">
           <input className="w-[1.5rem] h-[1.5rem] " type="checkbox" name="terms" checked={value} onChange={onChange} />
-          <label htmlFor="">I consent to being contracted by the team <span className="text-[#0C7D69]">*</span></label>
+          <label>I consent to being contracted by the team <span className="text-[#0C7D69]">*</span></label>
 
       </div>
       {errors.checkbox && <span className="text-[#D73C3C]">{errors.checkbox}</span>}
